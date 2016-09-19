2aad922212fe969521aacb008d8d2c421748b44b
lucas <lucasramela@hotmail.com>
2016-09-19 16:48:36 -0300

    Tocamos en archivo views.py agregando las vistas a mi proyecto.

--- cuentas/views.py
+++ cuentas/views.py
@@ -1,3 +1,24 @@
+import datetime
+from django.http import HttpResponse
+from django.http import Http404
 from django.shortcuts import render
+from cuentas.models import Cuenta
+
+
+def fecha(request):
+	now = datetime.datetime.now()
+#	html = "<html><body> Hoy es {0}.</body></html>".format(now)
+	html = "<html><body> El año es {0}.</body></html>".format(now.year)
+	return HttpResponse(html)
+
+def cuentas (request):
+	return render (request, 'cuentas.html', {'cuentas':Cuenta.objects.all(), 'total': Cuenta.objects.count()})
 
 # Create your views here.
+
+def cuenta (request, id):
+	try:
+		c = Cuenta.objects.get(pk=id)
+	except Cuenta.DoesNotExist:
+		raise Http404("No existe la cuenta seleccionada")
+	return render (request, 'cuenta.html',{'cuenta' : c})
