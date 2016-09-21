b6cfebc1ceb57bf24df5b55d7b2f78b5b942b8a6
lucas <lucasramela@hotmail.com>
2016-09-20 22:33:22 -0300

    Re loco todo, pocos cambios, agregamos limit y desde cuando. Y en form.as_p en form.py

--- cuentas/forms.py
+++ cuentas/forms.py
@@ -4,3 +4,6 @@ from django import forms
 
 class SearchForm(forms.Form):
 	query = forms.CharField(label='Busqueda',max_length=10)
+
+	limit = forms.IntegerField(label='Limite', min_value=1, required=False)
+	fecha = forms.DateTimeField(label='Desde cuando', required=False)
